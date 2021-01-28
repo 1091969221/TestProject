51a6ee73d3ed30aa089050e64a49ccb7d682ee71
lc-qy <1932158024@qq.com>
2021-01-28 15:37:46 +0800

    修改dao

--- src/main/java/xyz/yuluo/dao/TestDao.java
+++ src/main/java/xyz/yuluo/dao/TestDao.java
@@ -4,6 +4,11 @@ public class TestDao {
     public static void main(String[] args) {
         System.out.println("梦涵是个单身狗！");
         System.out.println("single dog! 涵儿");
+
     }
+
+     void dog() {
+         System.out.println("单身狗");
+     }
 
 }
